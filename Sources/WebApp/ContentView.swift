4f5f3d54f755242f71fef96175acb24d549e6639
ainame <s.namai.09@gmail.com>
2026-01-11 14:41:39 +0000

    Remove empty TotoViewModel.swift file

--- Sources/WebApp/ContentView.swift
+++ Sources/WebApp/ContentView.swift
@@ -15,34 +15,6 @@ final class TodoItem: Identifiable {
     }
 }
 
-@Reactive
-class TodoViewModel {
-    var items: [TodoItem] = []
-
-    func onAddNew() {
-        onAddNew(
-            TodoItem(
-                id: (items.last?.id ?? 0) + 1,
-                title: "Title",
-                description: "Description",
-                deadline: "2026-01-15"
-            )
-        )
-    }
-
-    func onAddNew(_ item: TodoItem) {
-        items.append(item)
-    }
-
-    func onDelete(at index: Int) {
-        items.remove(at: index)
-    }
-
-    func onUpdate(item: TodoItem, at index: Int) {
-        items[index] = item
-    }
-}
-
 @View
 struct ContentView {
     let viewModel = TodoViewModel()
