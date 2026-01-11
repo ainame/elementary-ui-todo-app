04569775f577314b707d5ca7c0a82caf717baaea
ainame <s.namai.09@gmail.com>
2026-01-11 12:12:19 +0000

    Introduce tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import swiftWasm from "@elementary-swift/vite-plugin-swift-wasm";
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    swiftWasm({
-      useEmbeddedSDK: true,
-    }),
-  ],
+    plugins: [
+        swiftWasm({
+            useEmbeddedSDK: true,
+        }),
+        tailwindcss(),
+    ],
 });
